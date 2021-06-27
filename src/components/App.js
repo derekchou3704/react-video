d6cd9d0b1874687540eff9d5328e9b4030f1e29b
DerekChou <derekchou3704@gmail.com>
2021-06-28 01:42:06 +0800

    Add the default search term and make the first video searched to be the default selectedVideo

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from './VideoDetail'
 class App extends React.Component {
     state = { videos: [], selectedVideo: null }
 
+    componentDidMount() {
+        this.onTermSubmit('youtube')
+    }
+
     onTermSubmit = async (term) => {
         try {
             const response = await youtube.get('/search', {
@@ -15,7 +19,10 @@ class App extends React.Component {
                 }
             })
 
-            this.setState({ videos: response.data.items })
+            this.setState({ 
+                videos: response.data.items,
+                selectedVideo: response.data.items[0]
+             })
         }
         catch (e) {
             console.log(e)
