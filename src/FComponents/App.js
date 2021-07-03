31c21de77ecaf77cf2d71ec4a2bca44a12feb7cd
DerekChou <derekchou3704@gmail.com>
2021-07-03 12:32:31 +0800

    Refactor the App component to functional component

--- src/FComponents/App.js
+++ src/FComponents/App.js
@@ -1,54 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
 import VideoList from './VideoList'
 import VideoDetail from './VideoDetail'
 
-class App extends React.Component {
-    state = { videos: [], selectedVideo: null }
+const App = () => {
+    const [videos, setVideos] = useState([])
+    const [selectedVideo, setSelectedVideo] = useState(null)
 
-    componentDidMount() {
-        this.onTermSubmit('youtube')
+    useEffect(() => {
+        onTermSubmit('youtube')        
+    }, [])
+
+    const onTermSubmit = async term => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        })
+        setVideos(response.data.items)
+        setSelectedVideo(response.datt.items[0])
     }
 
-    onTermSubmit = async (term) => {
-        try {
-            const response = await youtube.get('/search', {
-                params: {
-                    q: term
-                }
-            })
-
-            this.setState({ 
-                videos: response.data.items,
-                selectedVideo: response.data.items[0]
-             })
-        }
-        catch (e) {
-            console.log(e)
-        }
+    const onVideoSelect = video => {
+        setSelectedVideo(video)
     }
 
-    onVideoSelect = (video) => {
-        this.setState({ selectedVideo: video })
-    }
-
-    render() {
-        return(
-            <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit}/>
+    return () => {
+        <div className="ui container">
+                <SearchBar onFormSubmit={onTermSubmit}/>
                 <div className="ui grid">
                     <div className="ui row">
                         <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo} />
+                            <VideoDetail video={selectedVideo} />
                         </div>
                         <div className="five wide column">
-                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
                         </div>
                     </div>
                 </div>
             </div>
-        )
     }
 }
 
